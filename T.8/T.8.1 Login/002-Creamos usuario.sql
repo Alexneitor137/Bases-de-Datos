930a5b970d7a872bba00463cd4b5ae4c7a9f3549
Ajolote137 <Alexcalderonsanchez@gmail.com>
2025-12-05 10:34:12 +0100

    a

--- T.8/T.8.1 Login/002-Creamos usuario.sql
+++ T.8/T.8.1 Login/002-Creamos usuario.sql
@@ -2,22 +2,16 @@ CREATE USER
 'superaplicacion'@'localhost' 
 IDENTIFIED  BY 'Superaplicacion123$';
 
+GRANT USAGE ON *.* TO 'superaplicacion'@'localhost';
 
-GRANT USAGE ON *.* TO '[tunombredeusuario]'@'[tuservidor]';
---[tuservidor] == localhost
--- La contraseña puede requerir Mayus, minus, numeros, caracteres, min len
-
--- quitale todos los limites que tenga
-ALTER USER '[tunombredeusuario]'@'[tuservidor]' 
+ALTER USER 'superaplicacion'@'localhost' 
 REQUIRE NONE 
 WITH MAX_QUERIES_PER_HOUR 0 
 MAX_CONNECTIONS_PER_HOUR 0 
 MAX_UPDATES_PER_HOUR 0 
 MAX_USER_CONNECTIONS 0;
 
--- dale acceso a la base de datos empresadam
-GRANT ALL PRIVILEGES ON [tubasededatos].* 
-TO '[tunombredeusuario]'@'[tuservidor]';
+GRANT ALL PRIVILEGES ON superaplicacion.* 
+TO 'superaplicacion'@'localhost';
 
--- recarga la tabla de privilegios
 FLUSH PRIVILEGES;
