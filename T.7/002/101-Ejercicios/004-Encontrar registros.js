5b8d389493e271e5cef36258720b6a95b75da5aa
Ajolote137 <Alexcalderonsanchez@gmail.com>
2026-01-28 13:54:47 +0100

    s

---
+++ T.7/002/101-Ejercicios/004-Encontrar registros.js
@@ -0,0 +1,9 @@
+// Dame todos
+db.faturas.find()
+
+// Dame la primera
+db.facturas.findOne()
+
+// Select con where
+db.facturas.findOne({nombre:'Alejandro'})
+
---
+++ T.7/002/101-Ejercicios/005-Actualizar y eliminar.js
@@ -0,0 +1,8 @@
+// Actualizacion de un elemento
+db.facturas.updateOne(
+    {nombre:'Jose Vicente'},
+    {
+        $set:
+        {email:"prueba@prueba.com"}
+    }
+)
