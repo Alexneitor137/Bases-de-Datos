e056b3e360694d998aa41a1533b17d9f667e87d1
Ajolote137 <Alexcalderonsanchez@gmail.com>
2026-01-28 13:56:20 +0100

    Update 005-Actualizar y eliminar.js

--- T.7/002/101-Ejercicios/005-Actualizar y eliminar.js
+++ T.7/002/101-Ejercicios/005-Actualizar y eliminar.js
@@ -1,8 +1,22 @@
 // Actualizacion de un elemento
 db.facturas.updateOne(
-    {nombre:'Jose Vicente'},
+    {nombre:'Alejandro'},
     {
         $set:
         {email:"prueba@prueba.com"}
     }
+)
+
+// Actualizar muchos
+db.facturas.updateMany(
+    {email:'info@juan.com'},
+    {
+        $set:
+        {telefono:"11111111"}
+    }
+)
+
+// Eliminar uno
+db.facturas.deleteOne(
+    {nombre:'Alejandro'}
 )
